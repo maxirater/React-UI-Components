e0863ce3366431b36ec25a467fb3d5f210f17dba
Brad Cantrell <brad777mx@hotmail.com>
2019-03-25 17:50:20 -0700

    made pic clickable

--- Project-I-Social-Card/social-card/src/components/CardComponents/CardBanner.js
+++ Project-I-Social-Card/social-card/src/components/CardComponents/CardBanner.js
@@ -8,7 +8,9 @@ const CardBanner = () => {
     // const bannerImg = 'https://tk-assets.lambdaschool.com/fcd75197-7d12-46ec-bc9e-4130f34822fa_reactbackground.png';
 
     return (
+        <a href='https://reactjs.org/'>
       <img className='CardImage' src='https://tk-assets.lambdaschool.com/fcd75197-7d12-46ec-bc9e-4130f34822fa_reactbackground.png' alt='React Logo' />
+      </a>
     );
   };
   
